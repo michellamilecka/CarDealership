5f22f37b1d3f7fe58cf7895035b0b2aaf13e97b5
michella <michella.milecka@gmail.com>
2025-01-27 14:13:40 +0100

    updated new cars widget

--- salon_samochodowy/lib/views/widgets/new_car_widget.dart
+++ salon_samochodowy/lib/views/widgets/new_car_widget.dart
@@ -9,37 +9,55 @@ class NewCarsWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(),
-      child: Column(
-          children: [
-        Container(
-          child: Iconify(
-            Ph.car_simple_bold,
-            size: 96,
-          ),
-        ),
-        Text(
-          'Nowe samochody',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Colors.black,
-            fontFamily: 'Inter',
-            fontSize: 32,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        ElevatedButton(
-            onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context) => const NewCarsScreen(title: 'New Cars')));
-            },
-              style: ElevatedButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20), // Set the radius here
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(50.0),
+          child: Column(
+              children: [
+            Container(
+              child: Iconify(
+                Ph.car_simple_bold,
+                size: 80,
+                color: Colors.blue,
               ),
             ),
-            child: Text(
-              'Wyszukaj',
-            ))
-      ]),
+            Text(
+              'Nowe samochody',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Inter',
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+
+              ),
+            ),
+            SizedBox(
+              width: 150,
+              height: 45,
+              child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => const NewCarsScreen(title: 'New Cars')));
+                  },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xD9D9D9),
+
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(65), // Set the radius here
+                    ),
+                  ),
+                  child: Text(
+                    'Wyszukaj',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  )),
+            )
+          ]),
+        ),
+      ),
     );
   }
 }
